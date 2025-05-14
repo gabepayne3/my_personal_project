70915cf5b9ec2f753bb789b128a07cd2ff091429
Gabe Payne <gabepayne@Gabes-MacBook-Pro.local>
2025-05-14 16:14:43 +0100

    update p5

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
+
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE or DATABASE_URL not set")
@@ -20,4 +20,3 @@ if (ENV === "production") {
 }
 
 module.exports = new Pool(config);
-module.exports = db;
